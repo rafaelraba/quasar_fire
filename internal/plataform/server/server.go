2ec0de3ea042a3820c7aca6e4ba56d9df057c91c
Rafael Raba <rafaelrb40@gmail.com>
2021-02-28 17:41:59 -0500

    implement health_check

--- internal/plataform/server/server.go
+++ internal/plataform/server/server.go
@@ -1,30 +1,31 @@
 package server
 
 import (
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "log"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/rafaelraba/quasar_fire/internal/plataform/server/handler/health"
+	"log"
 )
 
 type Server struct {
-    httpAddr string
-    engine   *gin.Engine
+	httpAddr string
+	engine   *gin.Engine
 }
 
 func New(host string, port uint) Server {
-    srv := Server{
-        engine:   gin.New(),
-        httpAddr: fmt.Sprintf("%s:%d", host, port),
-    }
-    return srv
+	srv := Server{
+		engine:   gin.New(),
+		httpAddr: fmt.Sprintf("%s:%d", host, port),
+	}
+	srv.registerRoutes()
+	return srv
 }
 
 func (s *Server) Run() error {
-    log.Println("Server running on", s.httpAddr)
-    return s.engine.Run(s.httpAddr)
+	log.Println("Server running on", s.httpAddr)
+	return s.engine.Run(s.httpAddr)
 }
 
 func (s *Server) registerRoutes() {
-    s.engine.POST("")
+	s.engine.GET("/health", health.CheckHandler())
 }
-
